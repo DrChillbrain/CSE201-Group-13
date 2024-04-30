868579c84056b76886d5c06388051e3a79f0d3a9
jkapcheck <Kapcheja@miamioh.edu>
2024-04-30 19:19:58 -0400

    Added the top 5 users songs table to index page

--- music-database/routes/index.js
+++ music-database/routes/index.js
@@ -13,13 +13,58 @@ async function openDB() {
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  if (req.session.user) {
-    res.render('index', {
-      user: req.session.user,
-    });
-  } else {
-    res.render('index', {});
+  const db = await openDB();
+  const songsQuery = 'SELECT song_id FROM playlist_songs GROUP BY song_id ORDER BY COUNT(*)  DESC LIMIT 5';
+  const songsResults = await db.all(songsQuery, []);
+  const songsFinalQuery = 'SELECT name, genre, artist FROM songs WHERE id IN ($1, $2, $3, $4, $5)';
+  
+  if(!(songsResults.length == 5))
+  {
+    const songs2Query = 'SELECT id FROM songs WHERE id NOT IN (SELECT song_id FROM playlist_songs GROUP BY song_id ORDER BY COUNT(*) DESC LIMIT 5) ORDER BY id DESC LIMIT $1';
+    const songsNeeded = (5 - songsResults.length);
+    console.log(songsNeeded);
+    const songs2Results = await db.all(songs2Query, [songsNeeded]);
+    
+    let items = [];
+
+    for(let i = 0; i < songsResults.length; i++)
+    {
+      items[i] = songsResults[i].song_id;
+    }
+
+    for(let i = songsResults.length; i < 5; i++)
+    {
+      items[i] = songs2Results[i - songsResults.length].id;
+    }
+
+    let songsFinalResults = await db.all(songsFinalQuery, [items[0], items[1], items[2], items[3], items[4]]);
+
+    if (req.session.user) {
+      res.render('index', {
+        songsInList: songsFinalResults,
+        user: req.session.user, 
+      });
+    } 
+    else {
+      res.render('index', {songsInList: songsFinalResults,});
+    }
   }
+  else
+  {
+
+    let songsFinalResults = await db.all(songsFinalQuery, [songsResults[0].song_id, songsResults[1].song_id, songsResults[2].song_id, songsResults[3].song_id, songsResults[4].song_id]);
+
+    if (req.session.user) {
+      res.render('index', {
+        songsInList: songsFinalResults,
+        user: req.session.user, 
+      });
+    } 
+    else {
+      res.render('index', {songsInList: songsFinalResults,});
+    }
+  }
+
 });
 
 router.get('/search', async (req, res) => {
